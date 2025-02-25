aac1ffd157520b5b01640b33e198087cec94eae6
AldiGalang <121712363+AldiGalang@users.noreply.github.com>
2025-02-25 16:51:09 +0700

    Update script.sh

--- script.sh
+++ script.sh
@@ -9,4 +9,18 @@ mkdir -p $HOME/gaia-01
 curl -sSfL 'https://github.com/GaiaNet-AI/gaianet-node/releases/latest/download/install.sh' | bash -s -- --base $HOME/gaia-01
 
 # Load new environment variables
-source $HOME/.bashrc
+source script.sh
+
+# Initialize Gaia node
+gaianet init --base $HOME/gaia-01 --config https://raw.githubusercontent.com/GaiaNet-AI/node-configs/main/qwen2-0.5b-instruct/config.json
+gaianet config --base $HOME/gaia-01 --port 8101
+gaianet init --base $HOME/gaia-01
+
+# Kill any process using port 8101
+sudo lsof -t -i:8101 | xargs kill -9
+
+# Start Gaia node
+gaianet start --base $HOME/gaia-01
+
+# Get nodeID and deviceID
+gaianet info --base $HOME/gaia-01
