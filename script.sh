27df13be44dd1063b8f477cf446523af578ff386
AldiGalang <121712363+AldiGalang@users.noreply.github.com>
2025-02-25 16:46:39 +0700

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,1 +1,12 @@
-sudo apt update && sudo apt install git -y
+# Install Ubuntu updates
+sudo apt update && sudo apt upgrade -y
+sudo apt-get update && sudo apt-get upgrade -y
+
+# Create folder for Gaia node
+mkdir -p $HOME/gaia-01
+
+# Install Gaia node
+curl -sSfL 'https://github.com/GaiaNet-AI/gaianet-node/releases/latest/download/install.sh' | bash -s -- --base $HOME/gaia-01
+
+# Load new environment variables
+source $HOME/.bashrc
