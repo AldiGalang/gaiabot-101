52e2a49c7f2403ac9cbebe479308310657a26dae
AldiGalang <121712363+AldiGalang@users.noreply.github.com>
2025-02-27 19:30:46 +0700

    Update auto2.sh

--- script.sh
+++ script.sh
@@ -1,26 +1,20 @@
+#!/bin/bash
 # Install Ubuntu updates
 sudo apt update && sudo apt upgrade -y
 sudo apt-get update && sudo apt-get upgrade -y
-
 # Create folder for Gaia node
-mkdir -p $HOME/gaia-01
-
+mkdir -p $HOME/gaia-node-102
 # Install Gaia node
-curl -sSfL 'https://github.com/GaiaNet-AI/gaianet-node/releases/latest/download/install.sh' | bash -s -- --base $HOME/gaia-01
-
+curl -sSfL 'https://github.com/GaiaNet-AI/gaianet-node/releases/latest/download/install.sh' | bash -s -- --base $HOME/gaia-node-102
 # Load new environment variables
-source script.sh
-
+source $HOME/.bashrc
 # Initialize Gaia node
-gaianet init --base $HOME/gaia-01 --config https://raw.githubusercontent.com/GaiaNet-AI/node-configs/main/qwen2-0.5b-instruct/config.json
-gaianet config --base $HOME/gaia-01 --port 8101
-gaianet init --base $HOME/gaia-01
-
-# Kill any process using port 8101
-sudo lsof -t -i:8101 | xargs kill -9
-
+gaianet init --base $HOME/gaia-node-102 config --chat-url https://huggingface.co/gaianet/Llama-3.2-3B-Instruct-GGUF/resolve/main/Llama-3.2-3B-Instruct-Q5_K_M.gguf --prompt-template llama-3-chat
+gaianet config --base $HOME/gaia-node-102 --port 8102
+gaianet init --base $HOME/gaia-node-102
+# Kill any process using port 8102
+sudo lsof -t -i:8102 | xargs kill -9
 # Start Gaia node
-gaianet start --base $HOME/gaia-01
-
+gaianet start --base $HOME/gaia-node-102
 # Get nodeID and deviceID
-gaianet info --base $HOME/gaia-01
+gaianet info --base $HOME/gaia-node-102
